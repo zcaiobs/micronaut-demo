e6b514ce234b6aff9842bc2f5ca206f2ac4072ce
zcaiobs <zcaiobernardodasilva@gmail.com>
2021-05-30 22:44:41 -0300

    add validation

--- src/main/kotlin/com/example/controller/DemoController.kt
+++ src/main/kotlin/com/example/controller/DemoController.kt
@@ -1,13 +1,24 @@
 package com.example.controller
 
+import com.example.domain.DemoRequest
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/api")
 class DemoController {
 
     @Get("/hello")
-    fun helloWorld() : String {
-        return "Hello World"
+    fun getDemo() : String {
+        return "Carlos"
+    }
+
+    @Post("/send")
+    fun postDemo(@Body @Valid request: DemoRequest): Any {
+        return request
     }
 }
---
+++ src/main/kotlin/com/example/domain/DemoRequest.kt
@@ -0,0 +1,13 @@
+package com.example.domain
+
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
+data class DemoRequest(
+    @field:NotBlank val name: String,
+    @field:NotBlank @field:Email val email: String,
+    @field:Size(max = 10) val password: String
+)
